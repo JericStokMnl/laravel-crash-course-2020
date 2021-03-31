d7c9033c8478381bd50e122d88e5a4d573387ab8
JericStokMnl <jeric.epon@stok.se>
2021-03-31 15:00:51 +0800

    Fix migrate issue

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 @section('content')
     <div class="rounded shadow d-block p-4">
-        <form class="needs-validation mb-5" novalidate method="POST" action={{ route('post.store') }}>
-            @csrf
-            <div class="form-group mb-3">
-                <textarea type="text" name="body" rows="5" id="body" placeholder="Write a post" class="form-control form-control-lg{{ $errors->has('body') ? ' is-invalid' : (old('body') ? ' is-valid' : '') }}">{{ old('body') }}</textarea>
-                @error('body')
-                <div class="invalid-feedback">
-                    {{ $message }}
+        @auth
+            <form class="needs-validation mb-5" novalidate method="POST" action={{ route('post.store') }}>
+                @csrf
+                <div class="form-group mb-3">
+                    <textarea type="text" name="body" rows="5" id="body" placeholder="Write a post" class="form-control form-control-lg{{ $errors->has('body') ? ' is-invalid' : (old('body') ? ' is-valid' : '') }}">{{ old('body') }}</textarea>
+                    @error('body')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
-                @enderror
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        @endauth
 
         @forelse ($posts as $post)
             <div class="d-block py-3{{ $loop->last ? ' mb-3' : ''}}" role="alert">
