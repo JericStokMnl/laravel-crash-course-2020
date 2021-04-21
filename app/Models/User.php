f5e98f9b09515b819ab60279fcc7cc5ff1ba9012
JericStokMnl <jeric.epon@stok.se>
2021-04-21 14:58:28 +0800

    Received likes

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Like::class);
     }
+
+    public function receivedLikes() {
+        return $this->hasManyThrough(Like::class, Post::class);
+    }
 }
--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -3,7 +3,7 @@
 <div class="rounded shadow d-block p-4">
     <div class="d-block">
         <h1 class="text-success">{{ $user->name }}</h1>
-        <b>Posted: {{$posts->count()}} {{ Str::plural('post', $posts->count()) }}</b>
+        <b>Posted: {{$posts->count()}} {{ Str::plural('post', $posts->count()) }} and received: {{ $user->receivedLikes->count() }} {{ Str::plural('like', $user->receivedLikes->count() ) }} </b>
     </div>
     @forelse ($posts as $post)
         <x-post :post="$post" />
